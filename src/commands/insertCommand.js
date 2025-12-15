8e28859fff5ed9cace49b170e0ec79ae266baae3
deans-bradley <developer@deansbrad.com>
2025-12-15 09:29:22 +0200

    Add: Implement insert command setup for document uploads

---
+++ src/commands/insertCommand.js
@@ -0,0 +1,30 @@
+import chalk from 'chalk';
+import { COMMAND, ARGUMENT, OPTION } from '../constants/description.js';
+
+export function setupInsertCommand(program, insertManager) {
+  const insertCommand = program;
+  
+  insertCommand
+    .command('insert')
+    .description(COMMAND.INSERT)
+    .argument('<input>', ARGUMENT.INPUT)
+    .option('-b, --batch-size <size>', OPTION.BATCH_SIZE)
+    .option('-d, --database <name>', OPTION.DATABASE)
+    .action(async (input, options) => {
+      try {
+        const batchSize = parseInt(options.batchSize);
+        if (isNaN(batchSize) || batchSize < 1) {
+          console.error(chalk.red('Error: Batch size must be a positive number'));
+          process.exit(1);
+        }
+        
+        await insertManager.uploadDocuments(input, {
+          batchSize,
+          database: options.database
+        });
+      } catch (error) {
+        console.error(chalk.red('Error:'), error.message);
+        process.exit(1);
+      }
+    });
+}
