354c879a38f18a9e37c19d072d5af4cb429133de
deans-bradley <developer@deansbrad.com>
2025-12-15 09:14:03 +0200

    Update: Config to include version and batchSize

--- src/config/config.js
+++ src/config/config.js
@@ -8,12 +8,14 @@ const __dirname = dirname(__filename);
 _config({ path: resolve(__dirname, `./.env`), quiet: true });
 
 const config = {
+  version: '0.1.0',
   couchdb: {
     url: process.env.COUCHDB_URL,
     username: process.env.COUCHDB_USERNAME,
     password: process.env.COUCHDB_PASSWORD,
     database: process.env.COUCHDB_DATABASE,
-  }
+  },
+  batchSize: 100
 };
 
 export default config;
