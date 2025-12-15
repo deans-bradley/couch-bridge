1bd7bd2868acb4c25f337cd6444ace91b2f2a7d9
deans-bradley <developer@deansbrad.com>
2025-12-15 09:47:35 +0200

    Update: Bump version number

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 _config({ path: resolve(__dirname, `./.env`), quiet: true });
 
 const config = {
-  version: '0.1.1',
+  version: '0.2.0',
   couchdb: {
     url: process.env.COUCHDB_URL,
     username: process.env.COUCHDB_USERNAME,
