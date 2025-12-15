9daf11b40d670443ce322e05109b7e225c7eac31
deans-bradley <developer@deansbrad.com>
2025-12-15 09:33:28 +0200

    Add: Integrate WhereManager and setupWhereCommand for enhanced query capabilities

--- bin/cb.js
+++ bin/cb.js
@@ -6,9 +6,12 @@ import InsertManager from '../src/managers/InsertManager.js';
 import { setupInsertCommand } from '../src/commands/insertCommand.js';
 import DeleteManager from '../src/managers/DeleteManager.js';
 import { setupDeleteCommand } from '../src/commands/deleteCommand.js';
+import { setupWhereCommand } from '../src/commands/whereCommand.js';
+import WhereManager from '../src/managers/WhereManager.js';
 
 const insertManager = new InsertManager();
-const deleteManager = new DeleteManager(); 
+const deleteManager = new DeleteManager();
+const whereManager = new WhereManager();
 
 program
   .name('cb')
@@ -17,6 +20,7 @@ program
 
 setupInsertCommand(program, insertManager);
 setupDeleteCommand(program, deleteManager);
+setupWhereCommand(program, whereManager);
 
 program.parse(process.argv);
 
