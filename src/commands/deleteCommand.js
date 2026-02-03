aa644d6dfadffaa1710681b98701270b053181e1
deans-bradley <developer@deansbrad.com>
2026-02-03 11:26:03 +0200

    Fix: Correct command options formatting and improve delete function parameters

--- src/commands/deleteCommand.js
+++ src/commands/deleteCommand.js
@@ -1,17 +1,24 @@
 import chalk from 'chalk';
+import { Command } from 'commander';
 import { COMMAND, ARGUMENT, OPTION } from '../constants/description.js';
+import DeleteManager from '../managers/DeleteManager.js';
 
+/**
+ * @param {Command} program 
+ * @param {DeleteManager} deleteManager
+ */
 export function setupDeleteCommand(program, deleteManager) {
+  /** @type {Command} */
   const deleteCommand = program;
   
   deleteCommand
     .command('delete')
     .description(COMMAND.DELETE)
     .argument('<view>', ARGUMENT.VIEW)
-    .option('-k', '--key <key>', OPTION.KEY)
+    .option('-k, --key <key>', OPTION.KEY)
     .option('-d, --database <name>', OPTION.DATABASE)
     .option('-b, --batch <size>', OPTION.BATCH_SIZE, '100')
-    .action(async (input, options) => {
+    .action(async (view, options) => {
       try {
         const batchSize = parseInt(options.batch);
         if (isNaN(batchSize) || batchSize < 1) {
@@ -22,9 +29,10 @@ export function setupDeleteCommand(program, deleteManager) {
         console.log(chalk.yellow(`   Database: ${options.database || 'default from config'}`));
         console.log(chalk.yellow(`   Batch size: ${batchSize}`));
         
-        await deleteManager.delete(view, key, { 
-          batchSize,
-          database: options.database 
+        await deleteManager.delete(view, {
+          key: options.key,
+          batchSize: options.batchSize,
+          database: options.database
         });
       } catch (error) {
         console.error(chalk.red('Error:'), error.message);
