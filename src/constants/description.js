336c0b8862281e98e9e5450d3886b2d1b46b6be2
deans-bradley <developer@deansbrad.com>
2026-05-25 18:33:01 +0200

    Add: Include CONFIG command and additional options in description constants

--- src/constants/description.js
+++ src/constants/description.js
@@ -3,7 +3,8 @@
 const COMMAND = Object.freeze({
   DELETE: 'Delete documents from CouchDB by view',
   INSERT: 'Upload documents to CouchDB',
-  WHERE: 'Filter documents by a specific property value. If no value is provided, then all documents with the same value for the specified property will be returned'
+  WHERE: 'Filter documents by a specific property value. If no value is provided, then all documents with the same value for the specified property will be returned',
+  CONFIG: 'View or update CLI CouchDB configuration'
 });
 
 const ARGUMENT = Object.freeze({
@@ -17,7 +18,12 @@ const OPTION = Object.freeze({
   DATABASE: 'Database name (uses default from config if not specified)',
   GENERATE_IDS: 'Generate UUIDs for document _id fields',
   KEY: 'The key value to query the view',
-  PROPERTY_VALUE: 'The property value'
+  PROPERTY_VALUE: 'The property value',
+  COUCH_URL: 'CouchDB server URL',
+  DEFAULT_DATABASE: 'Default CouchDB database name',
+  COUCH_USERNAME: 'CouchDB username',
+  COUCH_PASSWORD: 'CouchDB password',
+  SHOW: 'Show current configuration without updating it'
 });
 
 export {
