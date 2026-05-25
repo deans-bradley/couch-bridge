db03a308da067f0bfc0da0643663d2c7751d321d
deans-bradley <developer@deansbrad.com>
2026-05-25 18:33:34 +0200

    Add: Implement pre-action hook to ensure configuration and validate required CouchDB settings

--- bin/cb.js
+++ bin/cb.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import { program } from 'commander';
-import config from '../src/config/config.js';
+import config, { ensureConfigFile, validateConfig } from '../src/config/config.js';
 import InsertManager from '../src/managers/InsertManager.js';
 import { setupInsertCommand } from '../src/commands/insertCommand.js';
 import DeleteManager from '../src/managers/DeleteManager.js';
 import { setupDeleteCommand } from '../src/commands/deleteCommand.js';
 import { setupWhereCommand } from '../src/commands/whereCommand.js';
 import WhereManager from '../src/managers/WhereManager.js';
+import { setupConfigCommand } from '../src/commands/configCommand.js';
 
 const insertManager = new InsertManager();
 const deleteManager = new DeleteManager();
@@ -18,9 +20,27 @@ program
   .description('Execute bulk operations in CouchDB')
   .version(config.version);
 
+program.hook('preAction', () => {
+  ensureConfigFile();
+
+  const commandName = program.args[0];
+  if (commandName === 'config') {
+    return;
+  }
+
+  const missing = validateConfig();
+  if (missing.length > 0) {
+    console.error(chalk.red('Missing required CouchDB configuration values:'));
+    missing.forEach(value => console.error(`  - ${value}`));
+    console.error(chalk.yellow('Please run `cb config --couch-url <url> --couch-username <user> --couch-password <pass> --default-database <db>` to set required settings.'));
+    process.exit(1);
+  }
+});
+
 setupInsertCommand(program, insertManager);
 setupDeleteCommand(program, deleteManager);
 setupWhereCommand(program, whereManager);
+setupConfigCommand(program);
 
 program.parse(process.argv);
 
